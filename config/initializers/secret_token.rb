dcaaa7862a76c216f194f30ffc94febcb6b8bbbb
Ravi mugdha <mugdha410@gmail.com>
2012-09-03 12:21:56 -0700

    Update author Initial checkin of rails + compass twitter bootstrap

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CShopping::Application.config.secret_token = '4e7e99db8868fc03cc5b16386424f5bfd44f7afbecc057585feb6c8a70ec844acdf848c110c5fa50553bd979cf59672b6b089af02f274a771be2a7a09be9ae95'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+CShopping::Application.config.session_store :cookie_store, key: '_c-shopping_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# CShopping::Application.config.session_store :active_record_store
